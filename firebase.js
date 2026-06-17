fef4809db041950307f7de3f8c369ea009e4691c
navera01 <naveraaalexa@gmail.com>
2026-06-17 19:42:37 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,13 +9,13 @@ onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBa_Ur6MleAv6XFzBnTFRlcX_H_VwSWV3U",
-  authDomain: "navera-store.firebaseapp.com",
-  projectId: "navera-store",
-  storageBucket: "navera-store.firebasestorage.app",
-  messagingSenderId: "606350193071",
-  appId: "1:606350193071:web:d11d9b9163de3716363f5a",
-  measurementId: "G-3JV2HQEYHB"
+apiKey: "AIzaSyBa_Ur6MleAv6XFzBnTFRlcX_H_VwSWV3U",
+authDomain: "navera-store.firebaseapp.com",
+projectId: "navera-store",
+storageBucket: "navera-store.firebasestorage.app",
+messagingSenderId: "606350193071",
+appId: "1:606350193071:web:d11d9b9163de3716363f5a",
+measurementId: "G-3JV2HQEYHB"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -24,17 +24,53 @@ const auth = getAuth(app);
 
 window.loginGoogle = async () => {
 
-try{
+try {
 
-const provider =
-new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 
 await signInWithPopup(
 auth,
 provider
 );
 
-}catch(err){
+} catch(err){
+
+alert(err.message);
+
+}
+
+};
+
+window.logoutGoogle = async () => {
+
+await signOut(auth);
+
+};
+
+onAuthStateChanged(auth,(user)=>{
+
+if(user){
+
+document.getElementById("userName").innerText =
+user.displayName;
+
+document.getElementById("userEmail").innerText =
+user.email;
+
+document.getElementById("userPhoto").src =
+user.photoURL;
+
+}else{
+
+document.getElementById("userName").innerText =
+"Belum Login";
+
+document.getElementById("userEmail").innerText =
+"-";
+
+}
+
+});}catch(err){
 
 alert(err.message);
 
